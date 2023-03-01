1d73201192fcf006123bd45d33db015e4dbae004
Krunoslav Peric <k86peric@gmail.com>
2023-03-01 20:06:38 +0000

    Funkcije i operatori

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 
-require './about.php';
-require_once './about.php';
+$a = 10;
+$b = 15;
+$c = "string1 ";
+$d = "string2 ";
+
+echo 'a+b= ', $a + $b, "\n";
+echo 'a-b= ', $a - $b, "\n";
+echo 'a*b= ', $a * $b, "\n";
+echo 'a/b= ', $a / $b, "\n";
+echo 'a%b= ', $a % $b, "\n";
+
+echo "\n";
+
+$f = $c . $d;
+
+echo 'konkatenacija: ', $f, "\n";
+echo "\n";
+echo 'kombinirano: ', $a += $b, "\n";
+echo "\n";
+echo 'usporedba: ', var_dump($a > $b), "\n";
+echo "\n";
+echo ++$a, "\n";
+echo --$b, "\n";
