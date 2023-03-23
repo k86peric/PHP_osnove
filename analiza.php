f2efd99bfbb521681dd89ae9951e5f3b06c04fa3
Krunoslav Peric <k86peric@gmail.com>
2023-03-23 20:02:38 +0000

    Parcijalni2

---
+++ analiza.php
@@ -0,0 +1,39 @@
+<?php
+
+function vowelCounter(string $word) {
+    $vowelCount = 0;
+    $vowel = ['a','e','i','o','u'];
+    
+    foreach ($vowel as $appearance) {
+                $vowelCount += substr_count(strtolower($word), $appearance);
+            }
+        return $vowelCount;
+    }
+
+$word = $_POST["word"];
+if(!$word) {
+    echo "<h3>Polje za unos prazno!</h3>\n";
+}
+
+else if(ctype_alpha($word)) {
+    $brSlova = strlen($word);
+    $brSam = vowelCounter($word);
+    $brSug = $brSlova - $brSam;
+
+    $wordsJson = file_get_contents('words.json');
+
+    $wordsArr = json_decode($wordsJson, true);
+
+    $wordsArr[] = [
+        'word' => $word,
+        'brSlova' => $brSlova,
+        'brSam' => $brSam,
+        'brSug' => $brSug
+    ];
+
+    $wordsJson = json_encode($wordsArr);
+
+    file_put_contents('words.json', $wordsJson);
+
+    header("Location: index.php");
+}
--- index.php
+++ index.php
@@ -1,11 +1,45 @@
-<?php
-
-session_start();
-
-$_SESSION['foo'] = 'bar';
-
-session_write_close();
-
-var_dump(
-    $_SESSION
-);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parcijalni2</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                <form action="analiza.php" method="POST">
+                <label>Upišite riječ:</label><br>
+                <input type="text" name="word"><br><br>
+                <input type="submit" value="pošalji">
+                </form>
+            </td>
+            
+            <td>
+                <table border = "1">
+                    <tr>
+                        <th>Riječ</th>
+                        <th>Broj slova</th>
+                        <th>Broj suglasnika</th>
+                        <th>Broj samoglasnika</th>
+                    </tr>
+                    <?php
+                    $wordsJson = file_get_contents('words.json');
+                    $wordsArr = json_decode($wordsJson, true);
+                        foreach ($wordsArr as $word) {
+                        echo    "<tr>";
+                        echo        "<td>{$word['word']}</td>";
+                        echo        "<td>{$word['brSlova']}</td>";
+                        echo        "<td>{$word['brSug']}</td>";
+                        echo        "<td>{$word['brSam']}</td>";
+                        echo    "</tr>";
+                        }
+                    ?>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
